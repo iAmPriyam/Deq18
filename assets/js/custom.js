e69b725a7051c1fb81ac02ffe21ec14b8c1d08e4
Aditya Priyam <apriyam9@gmail.com>
2018-02-06 12:19:48 +0530

    Countdown timer updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -206,7 +206,7 @@ function onTouchEnd(e){
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 	
-	$('#mu-event-counter').countdown('2018/02/23').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2018/02/16').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
